6f2e3e189fbdf21ec7be92460ce9c8642e423972
yesmia1 <167889214+yesmia1@users.noreply.github.com>
2025-09-01 16:30:34 +0800

    Create quick_image.py

---
+++ quick_image.py
@@ -0,0 +1,33 @@
+import os
+import firebase_admin
+from firebase_admin import credentials, messaging
+
+# 从环境变量读取 JSON 并写入临时文件
+json_content = os.environ.get("QUICKIMAGE_JSON")
+if not json_content:
+    raise ValueError("FIREBASE_JSON secret not set in GitHub Actions")
+
+with open("firebase.json", "w") as f:
+    f.write(json_content)
+
+# 初始化 Firebase
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+
+# 构建消息
+message = messaging.Message(
+    data={
+        "type": "wakeup1",
+        "content": "唤醒应用1"
+    },
+    android=messaging.AndroidConfig(
+        priority='high'
+    ),
+    topic="imp_news"
+)
+
+try:
+    response = messaging.send(message)
+    print('消息发送成功:', response)
+except Exception as e:
+    print('发送失败:', e)
